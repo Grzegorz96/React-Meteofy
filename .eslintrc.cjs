f8553bf6377a7649e7cddc025ba95e58a6328dbc
Grzegorz Strzeszewski <grzesstrzeszewski@gmail.com>
2026-09-20 12:36:20 +0200

    libs updates

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,10 +6,11 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'eslint-config-prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  settings: { react: { version: 'detect' } },
   plugins: ['react-refresh'],
   rules: {
     'react/jsx-no-target-blank': 'off',
